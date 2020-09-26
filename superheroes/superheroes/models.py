5b3a57e28f69bdd2fc9de942e850e98b4e79ebc6
Anton Arnaudov <antonarnaudov76@gmail.com>
2020-09-26 22:31:04 +0300

    Superheroes django project /contains dinamic database usage/

---
+++ superheroes/superheroes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    secret_identity = models.CharField(max_length=40)
+
+    class Meta:
+        db_table = 'superheroes'
