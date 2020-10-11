fd6fc0182974c4acc91ade687c5be1136a45df09
Anton7692 <70327277+Anton7692@users.noreply.github.com>
2020-10-11 14:38:31 +0300

    postgres connection

--- django101/django101/settings.py
+++ django101/django101/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'django101.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SoftUni-DjangoProjects-DB',
+        'USER': 'postgres',
+        'PASSWORD': 'toni1o00',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 # Password validation
