ff2c18aab98f8ea1f38fae282c4a62a7502991be
Anton Arnaudov <antonarnaudov76@gmail.com>
2020-10-27 14:10:17 +0200

    updates

--- ExamPrep/expenses_tracker/views.py
+++ ExamPrep/expenses_tracker/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from expenses_tracker.forms import ExpensesForm
+from expenses_tracker.models import Expenses
 from profile.models import Profile
 
 
@@ -9,19 +11,50 @@ from profile.models import Profile
 def home_page(request):
     profile = Profile.objects.all()
     if profile:
+
+        expenses = Expenses.objects.all()
+        if expenses:
+            context = {
+                'profile': profile,
+                'expenses': expenses,
+            }
+            return render(request, 'home-with-profile.html', context)
+
         context = {
-            'profile': profile
+            'profile': profile,
+
         }
-        return render(request, 'home-with-profile.html', context)
+
+        return render(request, 'home-with-profile-no-expense.html', context)
     return render(request, 'home-no-profile.html')
 
 
+def create_or_edit(request, expense, template_name):
+    if request.method == 'GET':
+        context = {
+            'form': ExpensesForm(instance=expense),
+        }
+        return render(request, f'{template_name}.html', context)
+    else:
+        form = ExpensesForm(request.POST, instance=expense)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home page')
+
+        context = {
+            'form': form,
+        }
+
+        return render(request, f'{template_name}.html', context)
+
+
 def expense_create(request):
-    return render(request, 'expense-create.html')
+    return create_or_edit(request, Expenses(), 'expense-create')
 
 
 def expense_edit(request, pk):
-    return render(request, 'expense-edit.html')
+    return create_or_edit(request, Expenses(pk=pk), 'expense-edit')
 
 
 def expense_delete(request, pk):
